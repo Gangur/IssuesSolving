ad1f3a4c05a93d5c2342ab9b495d4bcacaa1fb65
GangurDV <GangurDV@live.ru>
2024-03-31 15:50:28 +0200

    Benchmark

--- IssuesSolving/Benchmark.cs
+++ IssuesSolving/Benchmark.cs
@@ -6,10 +6,18 @@ namespace IssuesSolving
     [MemoryDiagnoser]
     public class Benchmark
     {
+        private readonly int _n = 5000;
+        private readonly int[] _jumps = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
         [Benchmark]
-        public void Run() {
-            WordBreakProblem.IsStringBreakTabulation("catsandogsareanimalscatsandogsareanimalscatsandogsareanimalscatsandogsareanimalscatsandogsareanimalscatsandogsareanimals",
-                new string[] { "cats", "dog", "sand", "and", "cat", "mals", "san", "dogs", "are", "animal", "ani", "og", "sar" });
+        public void FindMemoization()
+        {
+            WaysToClimbProblem.FindMemoization(_n, _jumps);
+        }
+
+        [Benchmark]
+        public void FindTabulation() {
+            WaysToClimbProblem.FindTabulation(_n, _jumps);
         }
     }
 }

6db711a59b5c21b70b9dab8e7dac12c9b3f29f7b
GangurDV <GangurDV@live.ru>
2024-04-10 10:51:16 +0200

    - Reverse LinkedList;

---
+++ IssuesSolving/LinkedLists/ReverseLinkedList9.cs
@@ -0,0 +1,25 @@
+﻿using IssuesSolving.LinkedLists.Structure;
+
+namespace IssuesSolving.LinkedLists
+{
+    public static class ReverseLinkedList9
+    {
+        public static LinkedList Reverse(LinkedList list)
+        {
+            Node previous = null,
+                current = list.head,
+                next = null;
+
+            while (current != null)
+            {
+                next = current.next;
+                current.next = previous;
+                previous = current;
+                current = next;
+            }
+
+            list.head = previous;
+            return list;
+        }
+    }
+}
--- IssuesSolving/Program.cs
+++ IssuesSolving/Program.cs
@@ -1,15 +1,15 @@
 ﻿using BenchmarkDotNet.Running;
 using IssuesSolving;
 using IssuesSolving.DynamicProgramming;
+using IssuesSolving.LinkedLists;
 using IssuesSolving.LinkedLists.Structure;
 using System.Collections.Generic;
 
 public static class Program
 {
-
+    //var summary = BenchmarkRunner.Run<Benchmark>();
     public static void Main(string[] args)
     {
-        var summary = BenchmarkRunner.Run<Benchmark>();
         //Console.WriteLine(WaysToClimbProblem.FindMemoization(12000, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }));
     }
 }
